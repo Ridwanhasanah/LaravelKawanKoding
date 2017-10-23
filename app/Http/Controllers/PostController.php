7a2ea9c118a7c47cab95b58a88109f214afdf90b
Ridwan Hasanah <sun.riseagain@yahoo.com>
2017-10-23 10:33:58 +0700

    membuat validasi form

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,27 +6,36 @@ use Illuminate\Http\Request;
 use App\Post;
 use App\Category;
 
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::orderBy('created_at', 'desc')->get();//all();
 
-class PostController extends Controller{
-
-  public function index(){
-    $posts = Post::orderBy('created_at', 'desc')->get();//all();
-
-    return view('post.index',compact('posts'));
-  }
-    public function create(){
-      $categories = Category::all();
+        return view('post.index', compact('posts'));
+    }
+    public function create()
+    {
+        $categories = Category::all();
 
 
-      return view('post.create', compact('categories'));
+        return view('post.create', compact('categories'));
     }
 
-    public function store(){
+    public function store()
+    {
+        /* Validasi Start*/
+        $this->validate(request(), [
+        'title'   => 'required|min:5',
+        'content' => 'required|min:50'
+      ]);
+        /* Validasi End*/
+
         Post::create([
           'title'       => request('title'),
           'slug'        => str_slug(request('title')),
           'content'     => request('content'),
-          'category_id' => request('category_id')
+           'category_id' => request('category_id')
         ]);
 
         return redirect()->route('post.index')->withInfo('Post Aded');//->with('success','Post aded');
@@ -34,34 +43,36 @@ class PostController extends Controller{
     }
 
     // ===== Edit
-    public function edit(Post $post){//($id){
-      //$post = Post::find($id); di gunakan jika tidak menggunakan data binding
-      $categories = Category::all();
+    public function edit(Post $post)
+    {//($id){
+        //$post = Post::find($id); di gunakan jika tidak menggunakan data binding
+        $categories = Category::all();
 
-      return view('post.edit', compact('post','categories'));
+        return view('post.edit', compact('post', 'categories'));
     }
 
-    public function update(Post $post){//($id){
-      //$post = Post::find($id);
+    public function update(Post $post)
+    {//($id){
+        //$post = Post::find($id);
 
-      $post->update([
+        $post->update([
         'title'       => request('title'),
         'category_id' => request('category_id'),
         'content'     => request('content')
       ]);
 
-      return redirect()->route('post.index')->withSuccess('Post Changed');
+        return redirect()->route('post.index')->withSuccess('Post Changed');
     }
 
     // Delete
-    public function destroy(Post $post){
-
-      $post->delete();
-      return redirect()->route('post.index')->withDanger('Post Deleted');
-
+    public function destroy(Post $post)
+    {
+        $post->delete();
+        return redirect()->route('post.index')->withDanger('Post Deleted');
     }
-
-    public function show(Post $post){
-      return view('post.show',compact('post'));
+    // Tampil
+    public function show(Post $post)
+    {
+        return view('post.show', compact('post'));
     }
 }
--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -5,9 +5,14 @@
 <div class="container">
   <form class="" action="{{ route('post.store') }}" method="post">
     {{ csrf_field() }}
-    <div class="from-group">
+    <div class="from-group has-feedback{{ $errors->has('title') ? 'has-error' : ''}}">
       <label for="">Title</label>
-      <input type="text" class="form-control" name="title" placeholder="Post title">
+      <input type="text" class="form-control" name="title" placeholder="Post title" value="{{ old('title')}}">
+      @if ($errors->has('title'))
+        <span class="help-block">
+          <p>{{ $errors ->first('title') }}</p>
+        </span>
+      @endif
     </div><br>
     <div class="from-group">
       <label for="">Category</label>
@@ -19,7 +24,14 @@
     </div>
     <div class="from-group">
       <label for="">Content</label>
-      <textarea name="content" rows="5" class="form-control" placeholder="Post content"></textarea>
+      <textarea name="content" class="form-control has-feedback{{ $errors->has('content') ? 'has-error' : ''}}" rows="5" placeholder="Post content">
+        {{ old('content')}}
+      </textarea>
+        @if ($errors->has('content'))
+          <span class="help-block">
+            <p>{{ $errors ->first('content') }}</p>
+          </span>
+        @endif
     </div><br>
     <div class="from-group">
       <input type="submit" name="" class="btn btn-primary" value="Save">
